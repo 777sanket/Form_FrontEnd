67deb1d81ff7b996eb3a0fc19511697c87c62b27
777sanket <sanketwasnik1902@gmail.com>
2025-01-03 23:50:35 +0530

    2nd commit

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,13 +1,13 @@
-import Logo from "../../components/logo/Logo"
-import { Link } from 'react-router-dom';
-import { useState, useEffect } from 'react';
-import styles from './HomePage.module.css';
-import homeSemiCircle from '../../assets/homeSemiCircle.png';
-import homeTriangle from '../../assets/homeTriangle.png';
-import leftCircle from '../../assets/leftCircle.png';
-import rightCircle from '../../assets/rightCircle.png';
-import Container from '../../assets/Container.png';
-import hyperlink from '../../assets/hyperlink.png';
+import Logo from "../../components/logo/Logo";
+import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
+import styles from "./homePage.module.css";
+import homeSemiCircle from "../../assets/homeSemiCircle.png";
+import homeTriangle from "../../assets/homeTriangle.png";
+import leftCircle from "../../assets/leftCircle.png";
+import rightCircle from "../../assets/rightCircle.png";
+import Container from "../../assets/Container.png";
+import hyperlink from "../../assets/hyperlink.png";
 export default function HomePage() {
   return (
     <>
@@ -32,22 +32,25 @@ export default function HomePage() {
           </div>
           <div className={styles.content}>
             <div className={styles.topContent}>
-              Build advanced chatbots
-            visually</div>
-            <div className={styles.bottomContent}>Typebot gives you powerful blocks to create unique chat experiences. Embed them
-            anywhere on your web/mobile apps and start collecting results like magic.</div>
-            <Link to="/login"> 
-              <button>Create a FormBot  for free</button>
+              Build advanced chatbots visually
+            </div>
+            <div className={styles.bottomContent}>
+              Typebot gives you powerful blocks to create unique chat
+              experiences. Embed them anywhere on your web/mobile apps and start
+              collecting results like magic.
+            </div>
+            <Link to="/login">
+              <button>Create a FormBot for free</button>
             </Link>
           </div>
-          <div className={styles.rightImg}> 
+          <div className={styles.rightImg}>
             <img src={homeSemiCircle} alt="" />
           </div>
         </div>
 
         <div className={styles.imgContainer}>
           <div className={styles.leftCircle}>
-            <img src={leftCircle  } alt="" />
+            <img src={leftCircle} alt="" />
           </div>
           <div className={styles.rightCircle}>
             <img src={rightCircle} alt="" />
@@ -59,7 +62,7 @@ export default function HomePage() {
 
         <div className={styles.footer}>
           <div className={styles.container}>
-            <div className={styles.div1}> 
+            <div className={styles.div1}>
               <Logo />
             </div>
             <div className={styles.div2}> Product</div>
@@ -69,11 +72,11 @@ export default function HomePage() {
               Made with ❤️ by
               <div className={styles.cuvetteLink}> @cuvette </div>
             </div>
-            <div className={styles.div6}> 
+            <div className={styles.div6}>
               <span className={styles.gridItem}>
                 Status <img src={hyperlink} alt="status icon" />
-               </span>
-            </div>     
+              </span>
+            </div>
             <div className={styles.div7}>
               <span className={styles.gridItem}>
                 Documentation <img src={hyperlink} alt="status icon" />
@@ -83,55 +86,54 @@ export default function HomePage() {
               <span className={styles.gridItem}>
                 Roadmap <img src={hyperlink} alt="status icon" />
               </span>
-
             </div>
-            <div className={styles.div9}> 
+            <div className={styles.div9}>
               <span className={styles.gridItem}>
                 Pricing <img src={hyperlink} alt="status icon" />
               </span>
             </div>
-            <div className={styles.div10}> 
+            <div className={styles.div10}>
               <span className={styles.gridItem}>
                 Discord <img src={hyperlink} alt="status icon" />
               </span>
             </div>
-            <div className={styles.div11}> 
-              <span className={styles.gridItem} >
+            <div className={styles.div11}>
+              <span className={styles.gridItem}>
                 GitHub Repository <img src={hyperlink} alt="status icon" />
               </span>
             </div>
             <div className={styles.div12}>
-              <span className={styles.gridItem} >
+              <span className={styles.gridItem}>
                 Twitter <img src={hyperlink} alt="status icon" />
               </span>
             </div>
             <div className={styles.div13}>
-            <span className={styles.gridItem} >
+              <span className={styles.gridItem}>
                 LinkedIn <img src={hyperlink} alt="status icon" />
               </span>
             </div>
             <div className={styles.div14}>
-              <span className={styles.gridItem} >
+              <span className={styles.gridItem}>
                 OSS Friends <img src={hyperlink} alt="status icon" />
               </span>
             </div>
-            <div className={styles.div15}> 
-              <span className={styles.gridItem} >
+            <div className={styles.div15}>
+              <span className={styles.gridItem}>
                 About <img src={hyperlink} alt="status icon" />
               </span>
             </div>
-            <div className={styles.div16}> 
-              <span className={styles.gridItem} >
+            <div className={styles.div16}>
+              <span className={styles.gridItem}>
                 Contact <img src={hyperlink} alt="status icon" />
               </span>
             </div>
             <div className={styles.div17}>
-              <span className={styles.gridItem} >
+              <span className={styles.gridItem}>
                 Terms of service <img src={hyperlink} alt="status icon" />
               </span>
             </div>
-            <div className={styles.div18}> 
-              <span className={styles.gridItem} >
+            <div className={styles.div18}>
+              <span className={styles.gridItem}>
                 Privacy Policy <img src={hyperlink} alt="status icon" />
               </span>
             </div>
@@ -139,5 +141,5 @@ export default function HomePage() {
         </div>
       </div>
     </>
-  )
+  );
 }
